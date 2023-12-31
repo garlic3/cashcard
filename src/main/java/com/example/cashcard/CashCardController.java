10c81dd1fd354892dd4c27560a3ca8bd263eb31a
garlic3 <yup_yup@naver.com>
2023-12-31 21:53:02 +0900

    Implementing POST

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,10 +1,7 @@
 package com.example.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -39,6 +36,10 @@ public class CashCardController {
 
     }
 
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(){
+        return null;
+    }
 
 
 }
--- src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -51,4 +51,20 @@ class CashCardApplicationTests {
 
     }
 
+    @Test
+    void shouldCreatedANewCashCard(){
+
+        CashCard newCashCard = new CashCard(null, 250.00);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+
+
+    }
+
+
+
+
+
 }
